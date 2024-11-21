01e7cf4eb0ab3a563e7c8db573c3ae5da5452f30
Kronos <joaosantosprofi@protonmail.com>
2024-11-21 22:32:46 +0000

    Changed Port

--- counter.js
+++ counter.js
@@ -56,7 +56,8 @@ app.get('/counter', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
